3d675bdf940f06f721777abd580ce32f809ffe3e
George Kankava <gkankava@uplandsoftware.com>
2024-03-14 17:51:01 +0400

    Testing reactor code flatMap

--- src/test/java/com/example/reactorexamples/ReactorExamplesApplicationTests.java
+++ src/test/java/com/example/reactorexamples/ReactorExamplesApplicationTests.java
@@ -3,9 +3,13 @@ package com.example.reactorexamples;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class ReactorExamplesApplicationTests {
@@ -111,6 +115,61 @@ class ReactorExamplesApplicationTests {
 		StepVerifier.create(nationalParkFlux)
 				.expectNext("Yellowstone", "Yosemite", "Zion")
 				.verifyComplete();
+	}
+
+	@Test
+	void distinct() {
+		Flux<String> animalFlux = Flux.just("dog", "cat", "bird", "dog", "bird", "anteater")
+				.distinct();
+
+		StepVerifier.create(animalFlux)
+				.expectNext("dog", "cat", "bird", "anteater")
+				.verifyComplete();
+
+	}
+
+	@Test
+	public void map() {
+		Flux<Player> playerFlux = Flux
+				.just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+				.map(n -> {
+					String[] split = n.split("\\s");
+					return new Player(split[0], split[1]);
+				});
+
+		StepVerifier.create(playerFlux)
+				.expectNext(new Player("Michael", "Jordan"))
+				.expectNext(new Player("Scottie", "Pippen"))
+				.expectNext(new Player("Steve", "Kerr"))
+				.verifyComplete();
+	}
+
+		private record Player(String firstName, String lastName) {
+	}
+
+	@Test
+	void flatMap() {
+		Flux<Player> playerFlux = Flux
+				.just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+				.flatMap(n -> Mono.just(n)
+					.map(p -> {
+						String [] split = p.split("\\s");
+						return new Player(split[0], split[1]);
+					})
+				.subscribeOn(Schedulers.parallel())
+				);
+
+		List<Player> playerList = Arrays.asList(
+				new Player("Michael", "Jordan"),
+				new Player("Scottie", "Pippen"),
+				new Player("Steve", "Kerr"));
+
+		StepVerifier.create(playerFlux)
+				.expectNextMatches(playerList::contains)
+				.expectNextMatches(playerList::contains)
+				.expectNextMatches(playerList::contains)
+				.verifyComplete();
+
 	}
 
 }
